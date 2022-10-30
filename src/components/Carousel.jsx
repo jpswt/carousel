a8b0052133c337239a6c7ebebc9044ea0aa286d4
jpswt <joel_swt@yahoo.com>
2022-10-29 22:45:30 -0500

    added buttons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from 'react';
 import { images } from '../components/Data';
+import { FaChevronCircleLeft } from 'react-icons/fa';
+import { FaChevronCircleRight } from 'react-icons/fa';
+import './Carousel.css';
 
 function Carousel() {
 	const [currentImg, setCurrentImg] = useState(0);
 
 	return (
 		<div className="container">
-			<div style={{ backgroundImage: `url(${images[currentImg].img})` }}></div>
+			<div
+				className="carousel"
+				style={{ backgroundImage: `url(${images[currentImg].src})` }}
+			>
+				<div className="left-col">
+					<FaChevronCircleLeft style={{ fontSize: '40px' }} />
+				</div>
+				<div className="center-col"></div>
+				<div className="right-col">
+					<FaChevronCircleRight style={{ fontSize: '40px' }} />
+				</div>
+			</div>
 		</div>
 	);
 }
